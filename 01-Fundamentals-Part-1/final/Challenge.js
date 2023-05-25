84a90ce7ab05629cbde5daa7326266a8c5b6c922
nvoostrom <nick.v.oostrom@gmail.com>
2023-05-25 07:43:11 +0200

    first commit

---
+++ 01-Fundamentals-Part-1/final/Challenge.js
@@ -0,0 +1,70 @@
+/*
+Coding Challenge #1 
+Mark and John are trying to compare their BMI (Body Mass Index), which is 
+calculated using the formula: 
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg 
+and height in meter). 
+
+Your tasks: 
+1. Store Mark's and John's mass and height in variables 
+2. Calculate both their BMIs using the formula (you can even implement both 
+versions) 
+3. Create a Boolean variable 'markHigherBMI' containing information about 
+whether Mark has a higher BMI than John. 
+
+Test data: 
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 
+m tall. 
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 
+m tall. 
+ 
+GOOD LUCK 😀 
+
+// const markMassDataSet1 = 78
+// const johnMassDataSet1 = 92
+// const markHeigtDataSet1 = 1.69
+// const johnHeigtDataSet1 = 1.95
+
+// const markMassDataSet2 = 95
+// const johnMassDataSet2 = 85
+// const markHeigtDataSet2 = 1.88
+// const johnHeigtDataSet2 = 1.76
+
+// const bmiMarkData1Exponatiotion = markMassDataSet1 / markHeigtDataSet1 ** 2
+// const bmiJohnData1Exponatiotion = johnMassDataSet1 / johnHeigtDataSet1 ** 2
+// const bmiMarkData2Exponatiotion = markMassDataSet2 / markHeigtDataSet2 ** 2
+// const bmiJohnData2Exponatiotion = johnMassDataSet2 / johnHeigtDataSet2 ** 2
+
+// const markHigherBmiData1Exponatiotion = bmiMarkData1Exponatiotion >= bmiJohnData1Exponatiotion
+// const markHigherBmiData2Exponatiotion = bmiMarkData2Exponatiotion >= bmiJohnData2Exponatiotion
+
+// console.log(bmiMarkData1Exponatiotion, bmiJohnData1Exponatiotion, bmiMarkData2Exponatiotion, bmiJohnData2Exponatiotion)
+// console.log('In data set 1 the BMI of Mark is higher than that of John ' + markHigherBmiData1Exponatiotion)
+// console.log('In data set 2 the BMI of Mark is higher than that of John ' + markHigherBmiData2Exponatiotion)
+
+
+// const bmiMarkData1 = markMassDataSet1 / (markHeigtDataSet1 * markHeigtDataSet1)
+// const bmiJohnData1 = johnMassDataSet1 / (johnHeigtDataSet1 * johnHeigtDataSet1)
+// const bmiMarkData2 = markMassDataSet2 / (markHeigtDataSet2 * markHeigtDataSet2)
+// const bmiJohnData2 = johnMassDataSet2 / (johnHeigtDataSet2 * johnHeigtDataSet2)
+
+// const markHigherBmiData1 = bmiMarkData1 >= bmiJohnData1
+// const markHigherBmiData2 = bmiMarkData2 >= bmiJohnData2
+
+// console.log(bmiMarkData1, bmiJohnData1, bmiMarkData2, bmiJohnData2)
+// console.log('In data set 1 the BMI of Mark is higher than that of John ' + markHigherBmiData1)
+// console.log('In data set 2 the BMI of Mark is higher than that of John ' + markHigherBmiData2)
+
+// const markMass = 78
+// const johnMass = 92
+// const markHeigt = 1.69
+// const johnHeigt = 1.95
+
+// const bmiMark = markMass / markHeigt ** 2
+// const bmiJohn = johnMass / (johnHeigt * johnHeigt)
+
+// const markHigherBmi = bmiMark >= bmiJohn
+
+// console.log(bmiMark, bmiJohn)
+// console.log('the BMI of Mark is higher than that of John ' + markHigherBmi)
+*/
---
+++ 01-Fundamentals-Part-1/final/script.js
@@ -0,0 +1,168 @@
+/*
+create variable
+let js = 'amazin';
+
+//create alert for js
+// if (js === 'amazing') {
+//     alert('javascript is fun');
+// } else {
+//     alert('you are wrong');
+// }
+
+//write to console
+console.log(40 + 8 + 23 - 10);
+console.log('9 + 9 =', 9 + 9);
+console.log('Johas')
+console.log(23)
+
+//creat variables
+let firstName = "Matilda"
+let prefix = "the"
+let lastName = "Greate"
+
+//write to console
+console.log(firstName + " " + prefix + " " + lastName)
+console.log(firstName + " " + prefix + " " + lastName)
+console.log(firstName + " " + prefix + " " + lastName)
+
+//best practis is not to use one word to descripe variable in this axaple chage is to let firstName = "bob"
+let name = "Bob"
+
+//you can aslo use snakecase like this to make variable, best practice in js is use camle case
+let first_name = "lee"
+
+//cant make variable like this
+// let new = "new product"
+// let function = "function created"
+// if you want to use new or function you can chose to add a _,$ or any other letter (not recomended) 
+
+let $function = 'function created'
+
+//all upper case for thigns you know wont change
+let PI = 3.1415;
+
+//be descriptive like the first two. dont use the second option
+let myFirstJob = "Programmer"
+let myCurrentJob = "Teacher"
+
+let job1 = "Programmer"
+let job2 = "Teacher"
+
+console.log(myFirstJob)
+*/
+
+
+/*
+Number datatype
+let numberDataType = 1234
+//Stirng datatype
+let stringDataType = "String"
+//Boolean datatype
+let booleanDataType = true
+//Undefined dataType
+let undefinedDataType
+//Null datatype
+
+//Symbol datatype
+
+//BigInt datatype
+
+//value is a type. the variable is NOT a type 
+
+let javascriptIsFun = true
+console.log(javascriptIsFun)
+// use type of to see what datatype it is
+console.log(typeof true)
+console.log(typeof 23)
+console.log(typeof 'String')
+
+//resign a varialbel. this is posible because javascripts dynamic typing see line 70
+javascriptIsFun = 'YES!'
+console.log(typeof javascriptIsFun)
+
+let year;
+
+console.log(year)
+console.log(typeof year)
+
+year = 1999
+console.log(year)
+console.log(typeof year)
+
+// this is a bug. this returns a object this should retun null. is not being fixed because legecy
+console.log(typeof null)
+*/
+
+/*
+//var is old way of defining variable
+var job = 'Programmer'
+job = 'Coder'
+
+//let variable can be change/mutated latter in the script 
+let age = 30
+age = 31
+
+//const should and can not change/mutated later on
+const birthYear = 2002
+// birthYear = 2001
+// const job
+
+//best practice always use cont only use let when you sure that is should change
+
+//declare without making variable. it is posible but prefirible dont us is 
+
+lastName = 'Oostrom'
+console.log(lastName)
+*/
+
+/*
+//math operators
+//defrents kind of opperators
+
+console.log(ageNick)
+console.log(ageSara)
+
+console.log(ageNick, ageSara)
+
+// a * means time, / meand devide and ** means to the power of (3 = 2 * 2 * 2)
+console.log(ageNick * 2, ageNick / 10, 2 ** 3)
+
+const firstName = "Nick"
+const prefix = "van"
+const lastName = "Oostrom"
+console.log(firstName + " " + prefix + " " + lastName)
+
+//typeof is also a opperator see line 72 to 93
+
+//assigment operators
+let x = 10 + 5 // is 15
+x += 10 // x = x + 10 = 25
+x *= 4 // x = x * 4 = 100
+x -= 10 // x = x -10 = 90
+x++ // x + 1 = 91
+x-- // x - 1 = 90
+console.log(x)
+
+//comparison opperator 
+console.log(ageNick < ageSara) // >, <, >=, <=
+console.log(ageSara >= 18)
+
+const isFullAge = ageSara >= 18
+
+*/
+/*
+const now = 2023
+const ageNick = now - 2002
+const ageSara = now - 2004
+console.log(now - 1991 > now - 2002)
+
+console.log(25 - 10 - 5)
+
+let x, y
+x = y = 25 - 10 - 5 // x = y = 10, y=10 x=10
+
+console.log(x, y)
+
+const avarageAge = (ageNick + ageSara) / 2
+console.log(ageNick, ageSara, avarageAge)
+*/
